4eab833e26d4a916ac1d69a2c1bba99529c3e183
Ketsia-a <iraketsia2001@gmail.com>
2021-01-26 06:13:08 +0200

    added a message in the subject

--- projectapp/email.py
+++ projectapp/email.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
-    subject = 'Welcome to the .... '
+    subject = 'Welcome to the Voting app '
     sender = 'iraketsia2001@gmail.com'
 
     #passing in the context vairables
