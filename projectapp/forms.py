1da7726d1be5c4285075d5dff54cab68176785d6
Ketsia-a <iraketsia2001@gmail.com>
2021-01-26 02:28:40 +0200

    added profile and project forms to be used to fill in/add info

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from .models import Project, Profile, Rate
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
@@ -8,3 +9,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['first_name','last_name','bio','profile_pic']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name','project_pic','description','link']
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        exclude = ['user','project']
